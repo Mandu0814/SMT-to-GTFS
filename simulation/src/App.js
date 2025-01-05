cbfc3c4034a44c510b2308b8f0d5afbede50da31
1023sherry <alstn3316@gmail.com>
2025-01-05 23:29:28 +0900

    'update'

--- simulation/src/App.js
+++ simulation/src/App.js
@@ -6,24 +6,24 @@ import Splash from "./components/Splash";
 import Trip from "./components/Trip";
 import "./css/app.css";
 
-// const fetchData = (FilE_NAME) => {
-//   const res = axios.get(
-//     `https://raw.githubusercontent.com/1023sherry/UAM_NEW/main/uam/src/data/${FilE_NAME}.json`
-//   );
-//   const data = res.then((r) => r.data);
-//   return data;
-// };
+const fetchData = (FilE_NAME) => {
+  const res = axios.get(
+    `https://raw.githubusercontent.com/1023sherry/SMT-to-GTFS/main/simulation/src/data/${FilE_NAME}.json`
+  );
+  const data = res.then((r) => r.data);
+  return data;
+};
 
 console.log("NODE_ENV:", process.env.NODE_ENV);
 
 
-const fetchData = (fileName) => {
-  const baseURL = process.env.NODE_ENV === "production"
-    ? `https://raw.githubusercontent.com/1023sherry/UAM_NEW/main/uam/src/data/`
-    : `${process.env.PUBLIC_URL}/data/`;
+// const fetchData = (fileName) => {
+//   const baseURL = process.env.NODE_ENV === "production"
+//     ? `https://raw.githubusercontent.com/1023sherry/UAM_NEW/main/uam/src/data/`
+//     : `${process.env.PUBLIC_URL}/data/`;
   
-  return axios.get(`${baseURL}${fileName}.json`).then((r) => r.data);
-};
+//   return axios.get(`${baseURL}${fileName}.json`).then((r) => r.data);
+// };
 
 
 const App = () => {
